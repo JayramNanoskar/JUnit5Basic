5e8db3accc8f2b8d9f2008aa0dae68ad4a0e4dbb
JayramNanoskar <jayram.n978@gmail.com>
2021-12-06 21:30:59 +0530

    Asserting exceptions with assertThrows

--- junit-5/src/main/java/com/jayram/util/MathUtils.java
+++ junit-5/src/main/java/com/jayram/util/MathUtils.java
@@ -10,5 +10,16 @@ public class MathUtils {
 		return Math.PI * radius * radius;
 	}
 	
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+	
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}
 	
 }
--- junit-5/src/test/java/com/jayram/util/MathUtilsTest.java
+++ junit-5/src/test/java/com/jayram/util/MathUtilsTest.java
@@ -17,6 +17,12 @@ class MathUtilsTest {
 	@Test
 	void testComputeCircleRadius() {
 		MathUtils mathUtils = new MathUtils();
-		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should returm right circle area"); //In case of failure we can see message
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area"); //In case of failure we can see message
+	}
+	
+	@Test
+	void testDivide() {
+		MathUtils mathUtils = new MathUtils();
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw");
 	}
 }
