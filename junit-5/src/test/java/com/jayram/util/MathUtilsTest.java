d3a128f5f7e0ba870e122fb4993c71caa8536859
JayramNanoskar <jayram.n978@gmail.com>
2021-12-06 22:21:42 +0530

    Using JUnit lifecycle hook annotations

--- junit-5/src/test/java/com/jayram/util/MathUtilsTest.java
+++ junit-5/src/test/java/com/jayram/util/MathUtilsTest.java
@@ -2,13 +2,32 @@ package com.jayram.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
+	private MathUtils mathUtils;
+	
+	@BeforeAll
+	static void beforeAllInit() {
+		System.out.println("This needs to run before all");
+	}
+	
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
+	
+	@AfterEach
+	void cleanUp() {
+		System.out.println("Cleaning up.....");
+	}
+	
 	@Test
 	void testAdd() {
-		MathUtils mathUtils = new MathUtils();
 		int expected = 4;
 		int actual = mathUtils.add(2, 2);
 		assertEquals(expected, actual);
@@ -16,13 +35,11 @@ class MathUtilsTest {
 
 	@Test
 	void testComputeCircleRadius() {
-		MathUtils mathUtils = new MathUtils();
 		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area"); //In case of failure we can see message
 	}
 	
 	@Test
 	void testDivide() {
-		MathUtils mathUtils = new MathUtils();
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw");
 	}
 }
